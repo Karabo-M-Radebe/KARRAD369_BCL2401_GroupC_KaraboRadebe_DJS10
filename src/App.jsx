7e0ba3ce5729c1bf23c56bee977362dfdfb76c4d
Karabo M. Radebe <156776468+Karabo-M-Radebe@users.noreply.github.com>
2024-06-12 22:07:15 +0200

    added a useEffect and fetched the data from a URL

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,14 @@ function App() {
   const [posts, setPosts] = useState()
   const [error, setError] = useState()
 
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/posts')
+    .then(response => response.json())
+    .then(data => setPosts(data))
+    .catch(error => {setError(error)})
+    
+  }, [])
+
   return (
     <>
       
