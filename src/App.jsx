bbbc6bea0ac701c7c04ac3036b35711b128697db
Karabo M. Radebe <156776468+Karabo-M-Radebe@users.noreply.github.com>
2024-06-13 16:04:22 +0200

    updated the useState arguments

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,21 @@
 import { useState, useEffect } from 'react'
 
 export default function App() {
-  const [posts, setPosts] = useState()
-  const [error, setError] = useState()
+  const [posts, setPosts] = useState([]);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
     const fetchPosts= async () => {
       try{
-        const response = await fetch('https://jsonplaceholder.typicode.com/posts')
-        if (!response.ok) {throw new Error(`Data fetching failed`)}
+        const response = await fetch('https://jsonplaceholder.typicode.com/posts');
+        if (!response.ok) {
+          throw new Error(`Data fetching failed`)
+        }
         
         const data = await response.json();
-        setPosts(data)
-      } catch (error) {setError(error.message)}
+        setPosts(data);
+      } catch (error) {
+        setError(error.message)}
     };
     fetchPosts();
   }, [])
@@ -21,7 +24,7 @@ export default function App() {
     <>
       <div className='posts'>
         
-        {error ? (<div style={{ color: "black"}}>Error: {error}</div>) : (
+        {error ? (<div className='error-message'>Data fetching failed...</div>) : (
           <ul style={{ listStyleType: "none", padding: 0 }}>
             <h1>Posts</h1>
             {posts.map((post, index) => (
